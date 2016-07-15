8c6ef3373f0890a9156d622ceb6359e70cedf784
Mikey Clarke <mikey@basekit.com>
2016-07-15 01:07:59 +0100

    Fix data types

--- src/DatabaseMigrations/Version20160714015600.php
+++ src/DatabaseMigrations/Version20160714015600.php
@@ -65,10 +65,10 @@ CREATE TABLE `terminal` (
   `updated` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `name` varchar(255) DEFAULT NULL,
   `ipAddress` varchar(255) DEFAULT NULL,
-  `hardwareId` int(11) DEFAULT NULL,
-  `softwareId` int(11) DEFAULT NULL,
-  `networkId` int(11) DEFAULT NULL,
-  `userId` int(11) DEFAULT NULL,
+  `hardwareId` varchar(15) DEFAULT NULL,
+  `softwareId` varchar(15) DEFAULT NULL,
+  `networkId` varchar(15) DEFAULT NULL,
+  `userId` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8
 SQL;
